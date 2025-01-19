ef9369cafd3959cbe12f665dd79562e0ce73bb0d
Nathan Boonkon <158584121+NathanB054@users.noreply.github.com>
2025-01-19 23:29:57 +0700

    แก้ router navbar

--- src/components/Nav/NavTop.vue
+++ src/components/Nav/NavTop.vue
@@ -19,7 +19,7 @@ const mobileSections = [
             { label: 'งานสร้างความตระหนักทรัพย์สินทางปัญญา', to: '/services/aai' },
             { label: 'การขอรับความคุ้มครองทรัพย์สินทางปัญญา', to: '/services/ipa' },
             { label: 'การใช้ประโยชน์ผลงานทรัพย์สินทางปัญญา', to: '/services/abi' },
-            { label: 'การยกระดับงานวิจัยและนวัตกรรม', to: 'services/upi' },
+            { label: 'การยกระดับงานวิจัยและนวัตกรรม', to: '/services/upi' },
             { label: 'ระบบสืบค้นฐานข้อมูลทรัพย์สินทางปัญญา มฟล', to: '/services/ipp' },
         ],
         isActive: (route) => route.path.startsWith('/service'),
