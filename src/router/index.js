f536accb123611696c4b2151119ba1b9be45bb30
Nathan Boonkon <158584121+NathanB054@users.noreply.github.com>
2025-02-01 00:29:57 +0700

    แก้โค้ดให้ดูง่าย หน้า index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '../stores/auth';
-import HomeView from '../views/HomeView.vue'
-import InnovationDetail from '@/views/InnovationDetailView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import HomeView from "../views/HomeView.vue";
+import InnovationDetail from "@/views/InnovationDetailView.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       // component: DataIpView,
       component: HomeView,
       // meta: { showNavTop: true, requiresAuth: true }
-      meta: { showNavTop: true, showNavFooter: true }
+      meta: { showNavTop: true, showNavFooter: true },
+    },
+    
+    // catch all 404
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: () => import("../views/NotFound.vue"),
+      meta: { showNavTop: true },
+    },
+
+    {
+      path: "/about",
+      name: "About",
+      component: () => import("../views/AboutView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/about',
-    name: 'About',
-      component: () => import('../views/AboutView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/login",
+      name: "Login",
+      component: () => import("../views/LoginView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/LoginView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/register",
+      name: "Register",
+      component: () => import("../views/RegisterView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/register',
-      name: 'Register',
-      component: () => import('../views/RegisterView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/innovation",
+      name: "Innovation",
+      component: () => import("../views/InnovationView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/innovation',
-      name: 'Innovation',
-      component: () => import('../views/InnovationView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/services/ipp",
+      name: "Table Research",
+      component: () => import("../views/TableResearchView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/services/ipp',
-      name: 'Table Research',
-      component: () => import('../views/TableResearchView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/regulation",
+      name: "Regulation",
+      component: () => import("../views/RegulationDemo.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/regulation',
-      name: 'Regulation',
-      component: () => import('../views/RegulationDemo.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
-    },
-    {
-      path: '/services/ipa',
-      name: 'Request for Coverage',
-      component: () => import('../views/RequestCoverageView.vue'),
+      path: "/services/ipa",
+      name: "Request for Coverage",
+      component: () => import("../views/RequestCoverageView.vue"),
       props: true,
-      meta: { showNavTop: true, showNavFooter: true }
+      meta: { showNavTop: true, showNavFooter: true },
     },
     // {
     //   path: '/services/ipa/:topicToggle',
@@ -64,133 +73,127 @@ const router = createRouter({
     //   meta: { showNavTop: true, showNavFooter: true }
     // },
     {
-      path: '/database',
-      name: 'Database IP MFU',
-      component: () => import('../views/DataIpView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/database",
+      name: "Database IP MFU",
+      component: () => import("../views/DataIpView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/contact',
-      name: 'Contact',
-      component: () => import('../views/ContactView.vue'),
-      meta: { showNavTop: true, showNavFooter: true }
+      path: "/contact",
+      name: "Contact",
+      component: () => import("../views/ContactView.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/detail/:id',
-      name: 'Innovation Detail',
+      path: "/detail/:id",
+      name: "Innovation Detail",
       component: InnovationDetail,
       props: true,
-      meta: { showNavTop: true, showNavFooter: true }
+      meta: { showNavTop: true, showNavFooter: true },
     },
     {
-      path: '/profile',
-      name: 'Profile',
-      component: () => import('../views/UserView/ProfileView.vue'),
-      meta: { showNavTop: true, showNavFooter: true, requiresAuth: true }
+      path: "/profile",
+      name: "Profile",
+      component: () => import("../views/UserView/ProfileView.vue"),
+      meta: { showNavTop: true, showNavFooter: true, requiresAuth: true },
     },
     {
-      path: '/message',
-      name: 'Message',
-      component: () => import('../views/UserView/MessageView.vue'),
-      meta: { showNavTop: true, showNavFooter: true, requiresAuth: true }
+      path: "/message",
+      name: "Message",
+      component: () => import("../views/UserView/MessageView.vue"),
+      meta: { showNavTop: true, showNavFooter: true, requiresAuth: true },
     },
     {
-      path: '/admin/',
-      name: 'Admin',
-      component: () => import('../views/AdminView/index.vue'),
-      meta: { requiresAuth: true, role: 'admin' },
+      path: "/admin/",
+      name: "Admin",
+      component: () => import("../views/AdminView/index.vue"),
+      meta: { requiresAuth: true, role: "admin" },
     },
     {
-      path: '/admin/userManage',
-      name: 'User Manage',
-      component: () => import('../views/AdminView/UserManageView.vue'),
-      meta: { requiresAuth: true, role: 'admin' },
+      path: "/admin/userManage",
+      name: "User Manage",
+      component: () => import("../views/AdminView/UserManageView.vue"),
+      meta: { requiresAuth: true, role: "admin" },
     },
     {
-      path: '/admin/documents',
-      name: 'Documents',
-      component: () => import('../views/AdminView/DocumentsView.vue'),
-      meta: { requiresAuth: true, role: 'admin' },
+      path: "/admin/documents",
+      name: "Documents",
+      component: () => import("../views/AdminView/DocumentsView.vue"),
+      meta: { requiresAuth: true, role: "admin" },
     },
     {
-      path: '/staff/',
-      name: 'Staff',
-      component: () => import('../views/StaffView/index.vue'),
+      path: "/staff/",
+      name: "Staff",
+      component: () => import("../views/StaffView/index.vue"),
       meta: {
         requiresAuth: true,
-        roles: ['staff', 'admin']
+        roles: ["staff", "admin"],
       },
     },
     {
-      path: '/staff/newPost',
-      name: 'New Post',
-      component: () => import('../views/StaffView/NewsPost.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/newPost",
+      name: "New Post",
+      component: () => import("../views/StaffView/NewsPost.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/researchManagement',
-      name: 'Research Management',
-      component: () => import('../views/StaffView/ResearchManagement.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/researchManagement",
+      name: "Research Management",
+      component: () => import("../views/StaffView/ResearchManagement.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/searchManagement',
-      name: 'Search Management',
-      component: () => import('../views/StaffView/SearchManagement.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/searchManagement",
+      name: "Search Management",
+      component: () => import("../views/StaffView/SearchManagement.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/regulationManagement',
-      name: 'Regulation Management',
-      component: () => import('../views/StaffView/RegulationManagement.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/regulationManagement",
+      name: "Regulation Management",
+      component: () => import("../views/StaffView/RegulationManagement.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/groupManagement',
-      name: 'Group Management',
-      component: () => import('../views/StaffView/GroupManagement.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/groupManagement",
+      name: "Group Management",
+      component: () => import("../views/StaffView/GroupManagement.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/CoverageManagement',
-      name: 'Coverage Management',
-      component: () => import('../views/StaffView/CoverageManagement.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/CoverageManagement",
+      name: "Coverage Management",
+      component: () => import("../views/StaffView/CoverageManagement.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
     {
-      path: '/staff/messageReply',
-      name: 'Message Reply',
-      component: () => import('../views/StaffView/MessageReply.vue'),
-      meta: { requiresAuth: true, roles: ['staff', 'admin'] },
+      path: "/staff/messageReply",
+      name: "Message Reply",
+      component: () => import("../views/StaffView/MessageReply.vue"),
+      meta: { requiresAuth: true, roles: ["staff", "admin"] },
     },
 
-   // catch all 404
-  {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: () => import('../views/NotFound.vue'),
-    meta: { showNavTop: true }
-  },
-  {
-    path: '/services/aai',
-    name: 'AwareAsset',
-    component: () => import('../views/AwareAssetInfo.vue'),
-    meta: { showNavTop: true, showNavFooter: true }
-  },
-  {
-    path: '/services/abi',
-    name: 'AssetBenefit',
-    component: () => import('../views/AssetBenefitInfo.vue'),
-    meta: { showNavTop: true, showNavFooter: true }
-  },
-  {
-    path: '/services/upi',
-    name: 'UpdateInnovation',
-    component: () => import('../views/UpdateInnovation.vue'),
-    meta: { showNavTop: true, showNavFooter: true }
-  },
-
+    {
+      path: "/services/aai",
+      name: "AwareAsset",
+      component: () => import("../views/AwareAssetInfo.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
+    },
+    {
+      path: "/services/abi",
+      name: "AssetBenefit",
+      component: () => import("../views/AssetBenefitInfo.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
+    },
+    {
+      path: "/services/upi",
+      name: "UpdateInnovation",
+      component: () => import("../views/UpdateInnovation.vue"),
+      meta: { showNavTop: true, showNavFooter: true },
+    },
   ],
+
+  // page to top when route change
   scrollBehavior(to, from, savedPosition) {
     // if there is a saved position, return it (for example, when using browser navigation buttons)
     if (savedPosition) {
@@ -206,12 +209,9 @@ const router = createRouter({
       }, 0);
     });
   },
-})
+});
 
-
-
-
-// requiresAuth: true 
+// requiresAuth: true
 
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
@@ -222,5 +222,4 @@ router.beforeEach((to, from, next) => {
   }
 });
 
-
-export default router
+export default router;
